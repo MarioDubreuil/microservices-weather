9f923a2d610a323fa70ccf616207e6e508e2a103
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-06-15 23:19:29 -0400

    start to implement microservice for the report

---
+++ Cloudweather.Report/BusinessLogic/WeatherReportAggregator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Cloudweather.Report.Config;
+using Cloudweather.Report.DataAccess;
+using Microsoft.Extensions.Options;
+
+namespace Cloudweather.Report.BusinessLogic
+{
+	public interface IWeatherReportAggregator
+    {
+		public Task<WeatherReport> BuildWeeklyReport(string zip, int days);
+    }
+
+	public class WeatherReportAggregator : IWeatherReportAggregator
+	{
+		private readonly IHttpClientFactory _http;
+		private readonly ILogger<WeatherReportAggregator> _logger;
+		private readonly WeatherDataConfig _weatherDataConfig;
+		private readonly WeatherReportDbContext _db;
+
+        public WeatherReportAggregator(
+			IHttpClientFactory http,
+			ILogger<WeatherReportAggregator> logger,
+			IOptions<WeatherDataConfig> weatherConfig,
+			WeatherReportDbContext db)
+        {
+			_http = http;
+			_logger = logger;
+			_weatherDataConfig = weatherConfig.Value;
+			_db = db;
+        }
+
+        public Task<WeatherReport> BuildWeeklyReport(string zip, int days)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
